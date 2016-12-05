4b6144b1926a488d31667529bfc022e1a4f1ce43
Hsifeulbhsifder <zaeemkmohamed@gmail.com>
2016-12-05 13:41:25 -0500

    Final

--- source.cpp
+++ source.cpp
@@ -43,23 +43,23 @@
  *  time to wait for in order to have uniform  loop times.
  */
 void ProcessTime(float lastTime, float targetMSPerFrame,
-				 int timer){
-	float elapsedMS = time1[timer] - lastTime;
-	if(elapsedMS < targetMSPerFrame) {
-		wait1Msec(targetMSPerFrame - elapsedMS);
-		float testElapsedMS = time1[timer] - lastTime;
+                 int timer){
+    float elapsedMS = time1[timer] - lastTime;
+    if(elapsedMS < targetMSPerFrame) {
+        wait1Msec(targetMSPerFrame - elapsedMS);
+        float testElapsedMS = time1[timer] - lastTime;
 
-		if(testElapsedMS > targetMSPerFrame) {
-			nxtDisplayString(0, "Long Wait");
-		} else {
-			do {
-				elapsedMS = time1[timer] - lastTime;
-			} while(elapsedMS <= targetMSPerFrame);
-		}
+        if(testElapsedMS > targetMSPerFrame) {
+            nxtDisplayString(0, "Long Wait");
+        } else {
+            do {
+                elapsedMS = time1[timer] - lastTime;
+            } while(elapsedMS <= targetMSPerFrame);
+        }
 
-	} else {
-		nxtDisplayString(0, "Missed Frame");
-	}
+    } else {
+        nxtDisplayString(0, "Missed Frame");
+    }
 }
 
 /** Author: Zaeem Mohamed
@@ -67,29 +67,29 @@ void ProcessTime(float lastTime, float targetMSPerFrame,
  *  control loop.
  */
 typedef struct{
-	float kp; // coefficient for the proportional term
-	float ki; // coefficient for the integral term
-	float kd; // coefficient for the derivative term
-	float target; // target set-point value
-	float dt; // delta time (PID loop time)
-	float e; // error (target - observed value)
-	float i; // accumulative error (sum of errors)
-	float d; // derivative of error (difference in error over time)
+    float kp; // coefficient for the proportional term
+    float ki; // coefficient for the integral term
+    float kd; // coefficient for the derivative term
+    float target; // target set-point value
+    float dt; // delta time (PID loop time)
+    float e; // error (target - observed value)
+    float i; // accumulative error (sum of errors)
+    float d; // derivative of error (difference in error over time)
 } zmPID;
 
 /** Author: Taylor Robertson
  *  A function which clamps a given value between to other values.
  */
 float Clamp(float min, float max, float x){
-	float y;
-	if(x < min){
-		y = min;
-	} else if(x > max){
-		y = max;
-	} else{
-		y = x;
-	}
-	return y;
+    float y;
+    if(x < min){
+        y = min;
+    } else if(x > max){
+        y = max;
+    } else{
+        y = x;
+    }
+    return y;
 }
 
 /** Author: Zaeem Mohamed
@@ -100,13 +100,13 @@ float Clamp(float min, float max, float x){
  *  u(t) = kP*e(t) + kI*integral(e(t)dt) + kD*(de(t)/dt)
  */
 float ClosedPIDStep(zmPID& pid, float obs){
-	float e = pid.target - obs;
-	pid.i += e * pid.dt;
-	pid.d = (e - pid.e) / pid.dt;
-	float u = pid.kp * e + pid.ki * pid.i + pid.kd * pid.d;
+    float e = pid.target - obs;
+    pid.i += e * pid.dt;
+    pid.d = (e - pid.e) / pid.dt;
+    float u = pid.kp * e + pid.ki * pid.i + pid.kd * pid.d;
 
-	pid.e = e;
-	return u / 100.0;
+    pid.e = e;
+    return u / 100.0;
 }
 
 /** Author: Zaeem Mohamed
@@ -115,46 +115,46 @@ float ClosedPIDStep(zmPID& pid, float obs){
  *  and provides the object to the ClosedPIDStep function.
  */
 void PIDStrum(float target, float kp, float ki,
-			  float kd, int speed){
-	float dt = 1.0 / PID_HZ;
-	float targetMSPerFrame = 1000.0 * dt;
-	float tp = (target<nMotorEncoder[STRUM_MOTOR]?-100:100);
-	float vel = speed / 100.0;
-	zmPID pid;
+              float kd, int speed){
+    float dt = 1.0 / PID_HZ;
+    float targetMSPerFrame = 1000.0 * dt;
+    float tp = (target<nMotorEncoder[STRUM_MOTOR]?-100:100);
+    float vel = speed / 100.0;
+    zmPID pid;
 
-	pid.kp = kp;
-	pid.ki = ki;
-	pid.kd = kd;
-	pid.target = target;
-	pid.dt = dt;
-	pid.e = pid.target - nMotorEncoder[STRUM_MOTOR];
-	pid.i = 0;
-	pid.d = pid.e / pid.dt;
-	bool zeroed = false;
-	time1[T3] = 0;
-	float lastTime = time1[T3];
-	while(fabs(pid.e) > E_THRES || fabs(pid.d) > D_THRES
-		  && !zeroed){
-		if(SensorValue(S2)){
-			zeroed = true;
-			nMotorEncoder[STRUM_MOTOR] = 0;
-		}
-		float u = ClosedPIDStep(pid, nMotorEncoder[STRUM_MOTOR]);
-		motor[STRUM_MOTOR] = vel * Clamp(-100, 100, tp + u);
+    pid.kp = kp;
+    pid.ki = ki;
+    pid.kd = kd;
+    pid.target = target;
+    pid.dt = dt;
+    pid.e = pid.target - nMotorEncoder[STRUM_MOTOR];
+    pid.i = 0;
+    pid.d = pid.e / pid.dt;
+    bool zeroed = false;
+    time1[T3] = 0;
+    float lastTime = time1[T3];
+    while(fabs(pid.e) > E_THRES || fabs(pid.d) > D_THRES
+          && !zeroed){
+        if(SensorValue(S2)){
+            zeroed = true;
+            nMotorEncoder[STRUM_MOTOR] = 0;
+        }
+        float u = ClosedPIDStep(pid, nMotorEncoder[STRUM_MOTOR]);
+        motor[STRUM_MOTOR] = vel * Clamp(-100, 100, tp + u);
 
-		//Time Processing
-		ProcessTime(lastTime, targetMSPerFrame, T3);
+        //Time Processing
+        ProcessTime(lastTime, targetMSPerFrame, T3);
 
-		float endTime = time1[T3];
-		float frameTimeMS = endTime - lastTime;
-		lastTime = endTime;
-		float hz = 1000.0 / frameTimeMS;
+        float endTime = time1[T3];
+        float frameTimeMS = endTime - lastTime;
+        lastTime = endTime;
+        float hz = 1000.0 / frameTimeMS;
 
-		//Frame logging
-		nxtDisplayString(6, "%.04f(%.02f)",
-						 frameTimeMS / 1000.0, hz);
-	}
-	motor[STRUM_MOTOR] = 0;
+        //Frame logging
+        nxtDisplayString(6, "%.04f(%.02f)",
+                         frameTimeMS / 1000.0, hz);
+    }
+    motor[STRUM_MOTOR] = 0;
 }
 
 /** Author: Zaeem Mohamed
@@ -169,57 +169,57 @@ void PIDStrum(float target, float kp, float ki,
  *  process is explained below.
  */
 void PIDStrumMiss(float target, float kp, float ki,
-			  	  float kd, int speed, float dy){
-	float sign = 1;
-	if(target > nMotorEncoder[STRUM_MOTOR]){
-		sign = -1;
-	}
-	float dt = 1.0 / PID_HZ;
-	float targetMSPerFrame = 1000.0 * dt;
-	float tp = (target<nMotorEncoder[STRUM_MOTOR]?-100:100);
-	float vel = speed / 100.0;
-	float dx = target - nMotorEncoder[STRUM_MOTOR];
-	zmPID pid;
+                  float kd, int speed, float dy){
+    float sign = 1;
+    if(target > nMotorEncoder[STRUM_MOTOR]){
+        sign = -1;
+    }
+    float dt = 1.0 / PID_HZ;
+    float targetMSPerFrame = 1000.0 * dt;
+    float tp = (target<nMotorEncoder[STRUM_MOTOR]?-100:100);
+    float vel = speed / 100.0;
+    float dx = target - nMotorEncoder[STRUM_MOTOR];
+    zmPID pid;
 
-	pid.kp = kp;
-	pid.ki = ki;
-	pid.kd = kd;
-	pid.target = target;
-	pid.dt = dt;
-	pid.e = dx;
-	pid.i = 0;
-	pid.d = pid.e / pid.dt;
-	bool zeroed = false;
-	time1[T3] = 0;
-	float lastTime = time1[T3];
-	while(fabs(pid.e) > E_THRES || fabs(pid.d) > D_THRES
-		  && !zeroed){
-		if(SensorValue(S2)){
-			zeroed = true;
-			nMotorEncoder[STRUM_MOTOR] = 0;
-		}
-		float x = nMotorEncoder[STRUM_MOTOR];
-		float u = ClosedPIDStep(pid, x);
-		motor[STRUM_MOTOR] = vel * Clamp(-100, 100, tp + u);
-		float alpha = x / dx;
-		if(alpha < 0 || alpha > 1) alpha -= 1;
-		float y2 = 100 * cos(PI * alpha);
-		motor[PULL_MOTOR] = sign * DY * ((int)(y2) - 30);
-		//Time Processing
-		ProcessTime(lastTime, targetMSPerFrame, T3);
+    pid.kp = kp;
+    pid.ki = ki;
+    pid.kd = kd;
+    pid.target = target;
+    pid.dt = dt;
+    pid.e = dx;
+    pid.i = 0;
+    pid.d = pid.e / pid.dt;
+    bool zeroed = false;
+    time1[T3] = 0;
+    float lastTime = time1[T3];
+    while(fabs(pid.e) > E_THRES || fabs(pid.d) > D_THRES
+          && !zeroed){
+        if(SensorValue(S2)){
+            zeroed = true;
+            nMotorEncoder[STRUM_MOTOR] = 0;
+        }
+        float x = nMotorEncoder[STRUM_MOTOR];
+        float u = ClosedPIDStep(pid, x);
+        motor[STRUM_MOTOR] = vel * Clamp(-100, 100, tp + u);
+        float alpha = x / dx;
+        if(alpha < 0 || alpha > 1) alpha -= 1;
+        float y2 = 100 * cos(PI * alpha);
+        motor[PULL_MOTOR] = sign * DY * ((int)(y2) - 30);
+        //Time Processing
+        ProcessTime(lastTime, targetMSPerFrame, T3);
 
 
-		float endTime = time1[T3];
-		float frameTimeMS = endTime - lastTime;
-		lastTime = endTime;
-		float hz = 1000.0 / frameTimeMS;
+        float endTime = time1[T3];
+        float frameTimeMS = endTime - lastTime;
+        lastTime = endTime;
+        float hz = 1000.0 / frameTimeMS;
 
-		//Frame logging
-		nxtDisplayString(6, "%.04f(%.02f)",
-						 frameTimeMS / 1000.0, hz);
-	}
-	motor[STRUM_MOTOR] = 0;
-	motor[PULL_MOTOR] = 0;
+        //Frame logging
+        nxtDisplayString(6, "%.04f(%.02f)",
+                         frameTimeMS / 1000.0, hz);
+    }
+    motor[STRUM_MOTOR] = 0;
+    motor[PULL_MOTOR] = 0;
 }
 
 /** Author: Zaeem Mohamed
@@ -228,40 +228,40 @@ void PIDStrumMiss(float target, float kp, float ki,
  *  motor in intervals of 90 and 180 degree turns.
  */
 void PIDChord(float target, float kp, float ki, float kd){
-	nMotorEncoder[CHORD_MOTOR] = 0;
-	float dt = 1.0 / PID_HZ;
-	float targetMSPerFrame = 1000.0 * dt;
-	float dx = target - 0;
-	float tp = (dx < 0 ? -100 : 100);
-	zmPID pid;
+    nMotorEncoder[CHORD_MOTOR] = 0;
+    float dt = 1.0 / PID_HZ;
+    float targetMSPerFrame = 1000.0 * dt;
+    float dx = target - 0;
+    float tp = (dx < 0 ? -100 : 100);
+    zmPID pid;
 
-	pid.kp = kp;
-	pid.ki = ki;
-	pid.kd = kd;
-	pid.target = target;
-	pid.dt = dt;
-	pid.e = dx;
-	pid.i = 0;
-	pid.d = pid.e / pid.dt;
+    pid.kp = kp;
+    pid.ki = ki;
+    pid.kd = kd;
+    pid.target = target;
+    pid.dt = dt;
+    pid.e = dx;
+    pid.i = 0;
+    pid.d = pid.e / pid.dt;
 
-	time1[T3] = 0;
-	float lastTime = time1[T3];
-	while(fabs(pid.e) > E_THRES || fabs(pid.d) > D_THRES){
-		float u = ClosedPIDStep(pid, nMotorEncoder[CHORD_MOTOR] % 360);
-		motor[CHORD_MOTOR] = Clamp(-100, 100, tp + u);
-		//Time Processing
-		ProcessTime(lastTime, targetMSPerFrame, T3);
+    time1[T3] = 0;
+    float lastTime = time1[T3];
+    while(fabs(pid.e) > E_THRES || fabs(pid.d) > D_THRES){
+        float u = ClosedPIDStep(pid, nMotorEncoder[CHORD_MOTOR] % 360);
+        motor[CHORD_MOTOR] = Clamp(-100, 100, tp + u);
+        //Time Processing
+        ProcessTime(lastTime, targetMSPerFrame, T3);
 
-		float endTime = time1[T3];
-		float frameTimeMS = endTime - lastTime;
-		lastTime = endTime;
-		float hz = 1000.0 / frameTimeMS;
+        float endTime = time1[T3];
+        float frameTimeMS = endTime - lastTime;
+        lastTime = endTime;
+        float hz = 1000.0 / frameTimeMS;
 
-		//Frame logging
-		nxtDisplayString(6, "%.04f(%.02f)",
-						 frameTimeMS / 1000.0, hz);
-	}
-	motor[CHORD_MOTOR] = 0;
+        //Frame logging
+        nxtDisplayString(6, "%.04f(%.02f)",
+                         frameTimeMS / 1000.0, hz);
+    }
+    motor[CHORD_MOTOR] = 0;
 
 }
 
@@ -269,13 +269,13 @@ void PIDChord(float target, float kp, float ki, float kd){
  *  A struct that stores the data and meta data for a song.
  */
 typedef struct{
-	int size; // size of the song in bytes
-	int tempo; // tempo of the song
+    int size; // size of the song in bytes
+    int tempo; // tempo of the song
 
-	// Data of the song. Each char is 8 bits, where the first 2 bits
-	// store which chord is needed to played and the remaining 6 bits
-	// store the velocity of the strum.
-	char frames[MAX_SONG_SIZE];
+    // Data of the song. Each char is 8 bits, where the first 2 bits
+    // store which chord is needed to played and the remaining 6 bits
+    // store the velocity of the strum.
+    char frames[MAX_SONG_SIZE];
 } SongData;
 
 /** Author: Taylor Robertson
@@ -283,14 +283,14 @@ typedef struct{
  *  position except the chord motor.
  */
 void Callibrate(){
-	while(!SensorValue(S2)) motor[STRUM_MOTOR] = -15;
-	motor[STRUM_MOTOR] = 0;
-	motor[PULL_MOTOR] = -15;
-	wait1Msec(200);
-	motor[PULL_MOTOR] = 0;
-	nMotorEncoder[PULL_MOTOR] = 0;
-	nMotorEncoder[STRUM_MOTOR] = 0;
-	nMotorEncoder[CHORD_MOTOR] = 0;
+    while(!SensorValue(S2)) motor[STRUM_MOTOR] = -15;
+    motor[STRUM_MOTOR] = 0;
+    motor[PULL_MOTOR] = -15;
+    wait1Msec(200);
+    motor[PULL_MOTOR] = 0;
+    nMotorEncoder[PULL_MOTOR] = 0;
+    nMotorEncoder[STRUM_MOTOR] = 0;
+    nMotorEncoder[CHORD_MOTOR] = 0;
 }
 
 /** Author: Ben Krowchuk
@@ -299,8 +299,8 @@ void Callibrate(){
  *  some bit shifting and bit masking operations.
  */
 void GetFrameData(char byte, int& chord, int& strum){
-	chord = (int)((byte >> 6) & 0x03);
-	strum = 2 * (int)((byte & 0x3F));
+    chord = (int)((byte >> 6) & 0x03);
+    strum = 2 * (int)((byte & 0x3F));
 }
 
 /** Author: Andrew Xu
@@ -309,19 +309,19 @@ void GetFrameData(char byte, int& chord, int& strum){
  *  object by doing more bit shifts and bit masks.
  */
 void GetSongData(TFileHandle fin, SongData &data){
-	data.size = 0;
-	int tempo;
-	readIntPC(fin, tempo);
-	data.tempo = tempo;
-	int temp = 0;
-	while(readIntPC(fin, temp)){
-		data.frames[data.size] =  (char)((0x03 &
-								  (char)temp) << 6);
-		readIntPC(fin, temp);
-		data.frames[data.size] += (char)((0x3F &
-								  (char)(temp / 2)));
-		data.size++;
-	}
+    data.size = 0;
+    int tempo;
+    readIntPC(fin, tempo);
+    data.tempo = tempo;
+    int temp = 0;
+    while(readIntPC(fin, temp)){
+        data.frames[data.size] =  (char)((0x03 &
+                                  (char)temp) << 6);
+        readIntPC(fin, temp);
+        data.frames[data.size] += (char)((0x3F &
+                                  (char)(temp / 2)));
+        data.size++;
+    }
 }
 
 /** Author: Andrew Xu
@@ -330,14 +330,14 @@ void GetSongData(TFileHandle fin, SongData &data){
  *  achieve this angle.
  */
 void HoldChord(int chordData, int lastChordData){
-	int modChord = (chordData - lastChordData) % 4 - 2;
-	if(modChord == 1){
-		PIDChord(90, KPc, KIc, KDc);
-	} else if(modChord == -1 || modChord == -5){
-		PIDChord(-90, KPc, KIc, KDc);
-	} else if(modChord == 0){
-		PIDChord(180, KPc, KIc, KDc);
-	}
+    int modChord = (chordData - lastChordData) % 4 - 2;
+    if(modChord == 1){
+        PIDChord(90, KPc, KIc, KDc);
+    } else if(modChord == -1 || modChord == -5){
+        PIDChord(-90, KPc, KIc, KDc);
+    } else if(modChord == 0){
+        PIDChord(180, KPc, KIc, KDc);
+    }
 }
 
 /** Author: Ben Krowchuk
@@ -347,14 +347,14 @@ void HoldChord(int chordData, int lastChordData){
  *  once more.
  */
 void Strum(int speed){
-	if(speed > 10){
-		PIDStrum(STRUM_RANGE, KPs, KIs, KDs, speed);
-		PIDStrumMiss(0, KPs, KIs, KDs, speed * 0.25, DY);
-		PIDStrum(STRUM_RANGE, KPs, KIs, KDs, speed);
-		PIDStrum(0, KPs, KIs, KDs, speed);
-		PIDStrum(STRUM_RANGE, KPs, KIs, KDs, speed);
-		PIDStrumMiss(0, KPs, KIs, KDs, speed * 0.25, DY);
-	}
+    if(speed > 10){
+        PIDStrum(STRUM_RANGE, KPs, KIs, KDs, speed);
+        PIDStrumMiss(0, KPs, KIs, KDs, speed * 0.25, DY);
+        PIDStrum(STRUM_RANGE, KPs, KIs, KDs, speed);
+        PIDStrum(0, KPs, KIs, KDs, speed);
+        PIDStrum(STRUM_RANGE, KPs, KIs, KDs, speed);
+        PIDStrumMiss(0, KPs, KIs, KDs, speed * 0.25, DY);
+    }
 }
 
 //NOTE: This is global because there
@@ -380,56 +380,56 @@ static SongData songData;
  *  when the back button is pressed (default).
  */
 task main(){
-	SensorType[S2] = sensorTouch;
-	bMotorReflected[motorB] = true;
-	bMotorReflected[motorC] = true;
-	Callibrate();
-	wait1Msec(1000);
-	TFileHandle fin;
-	if(openReadPC(fin, "4chords.txt")){
-		nxtDisplayString(5, "We Good");
-		int strum, chord, lastChord = 0;
-		GetSongData(fin, songData);
-		float targetMSPerFrame = 60000.0 / (songData.tempo);
-		for(;;){
-			if(nNxtButtonPressed == 1){
-				PIDChord(-90, KPc, KIc, KDc);
-				Strum(100);
-			}else if(nNxtButtonPressed == 2){
-				PIDChord(90, KPc, KIc, KDc);
-				Strum(100);
-			}else if(nNxtButtonPressed == 3){
+    SensorType[S2] = sensorTouch;
+    bMotorReflected[motorB] = true;
+    bMotorReflected[motorC] = true;
+    Callibrate();
+    wait1Msec(1000);
+    TFileHandle fin;
+    if(openReadPC(fin, "4chords.txt")){
+        nxtDisplayString(5, "We Good");
+        int strum, chord, lastChord = 0;
+        GetSongData(fin, songData);
+        float targetMSPerFrame = 60000.0 / (songData.tempo);
+        for(;;){
+            if(nNxtButtonPressed == 1){
+                PIDChord(-90, KPc, KIc, KDc);
+                Strum(100);
+            }else if(nNxtButtonPressed == 2){
+                PIDChord(90, KPc, KIc, KDc);
+                Strum(100);
+            }else if(nNxtButtonPressed == 3){
 
-				time1[T1] = 0;
-				float lastTime = time1[T1];
-				for(int i = 0; i < songData.size; i++){
-					GetFrameData(songData.frames[i],
-								 chord, strum);
+                time1[T1] = 0;
+                float lastTime = time1[T1];
+                for(int i = 0; i < songData.size; i++){
+                    GetFrameData(songData.frames[i],
+                                 chord, strum);
 
-					//Process Input
-					HoldChord(chord, lastChord);
-					lastChord = chord;
-					//Strum(frame.strumData);
-					Strum(strum);
+                    //Process Input
+                    HoldChord(chord, lastChord);
+                    lastChord = chord;
+                    //Strum(frame.strumData);
+                    Strum(strum);
 
-					//Time Processing
-					ProcessTime(lastTime, targetMSPerFrame, T1);
+                    //Time Processing
+                    ProcessTime(lastTime, targetMSPerFrame, T1);
 
-					float endTime = time1[T1];
-					float frameTimeMS = endTime - lastTime;
-					lastTime = endTime;
-					float bpm = 60000.0 / frameTimeMS;
+                    float endTime = time1[T1];
+                    float frameTimeMS = endTime - lastTime;
+                    lastTime = endTime;
+                    float bpm = 60000.0 / frameTimeMS;
 
-					//Frame logging
-					nxtDisplayString(7, "%.02f(%.02f)",
-									 frameTimeMS
-									 / 1000.0, bpm);
-				}
-			}
-		}
+                    //Frame logging
+                    nxtDisplayString(7, "%.02f(%.02f)",
+                                     frameTimeMS
+                                     / 1000.0, bpm);
+                }
+            }
+        }
 
-	}else{
-		nxtDisplayString(0, "Failure");
-	}
+    }else{
+        nxtDisplayString(0, "Failure");
+    }
 
 }
